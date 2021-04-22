2d744b9086458a972e87d6a1d31baac0f58ac14f
tomhanna-uh <tlhanna@uh.edu>
2021-04-22 15:34:12 -0500

    getting acquainted 1

--- mainscript.R
+++ mainscript.R
@@ -1,6 +1,27 @@
 options(scipen = 999)
 library(here)
 
+library(car)
+library(ggplot2)
+library(stargazer)
+# library(Hmisc)
+library(coefplot)
+library(countrycode)
+
 library(readr)
 bismain <- read_csv(here("data","WEBSTATS_LBS_D_PUB_DATAFLOW_csv_col.csv"))
 View(bismain)
+
+#review variable names
+names(bismain)
+
+#get possible values for counterparty sector
+unique(bismain$`Counterparty sector`)
+
+bisarc <- bismain[ which(bismain$`Reporting country` == "All reporting countries"), ]
+
+summary(bisarc$`Counterparty sector`)
+
+
+      
+
